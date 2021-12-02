2d9ffd3cf4f5a42b9aa384c5992ca99c0942bd65
kshitiz <kshitiz@bambuser.com>
2021-12-02 13:44:36 +0100

    Use SF symbol as profile picture palceholder icon

--- NepTalk/Controllers/Login/RegisterViewController.swift
+++ NepTalk/Controllers/Login/RegisterViewController.swift
@@ -18,7 +18,7 @@ class RegisterViewController: UIViewController {
     
     private let imageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "person")
+        imageView.image = UIImage(systemName: "person.circle")
         imageView.tintColor = UIColor(named: "brand-blue")
         imageView.contentMode = .scaleAspectFit
         imageView.layer.masksToBounds = true
