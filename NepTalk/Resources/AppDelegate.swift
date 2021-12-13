62324d24236e9f2c69b79fd5a556d6f5d7ca5c0b
kshitiz <kshitiz@bambuser.com>
2021-12-13 20:52:17 +0100

    Configure google sign-in

--- NepTalk/Resources/AppDelegate.swift
+++ NepTalk/Resources/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FBSDKCoreKit
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -32,6 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
             annotation: options[UIApplication.OpenURLOptionsKey.annotation]
         )
+        return GIDSignIn.sharedInstance.handle(url)
     }
 }
 
